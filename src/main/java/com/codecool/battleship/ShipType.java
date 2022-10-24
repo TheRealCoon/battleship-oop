c4f4ba63977dcb330aaf421b38d5ff6bd5b046d9
T.Sanyi <s.truczkai@gmail.com>
2022-10-24 20:11:17 +0200

    added ShipType enum

---
+++ src/main/java/com/codecool/battleship/ShipType.java
@@ -0,0 +1,10 @@
+package com.codecool.battleship;
+
+public enum ShipType {
+    CARRIER(5), BATTLESHIP(4), CRUISER(3), SUBMARINE(2), DESTROYER(1);
+    private int length;
+
+    ShipType(int length) {
+        this.length = length;
+    }
+}
