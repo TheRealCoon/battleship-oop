6acdf3f3cc4015ab73a9bed46674bdb3bde422c1
T.Sanyi <s.truczkai@gmail.com>
2022-10-24 19:16:00 +0200

    added Battleship class

---
+++ src/main/java/com/codecool/battleship/Battleship.java
@@ -0,0 +1,24 @@
+package com.codecool.battleship;
+
+public class Battleship {
+    private Display display;
+    private Input input;
+    //TODO List<Score> for highschore (Score is a PlayerName - Integer pair)
+
+    public Battleship() {
+        display = new Display();
+        input = new Input();
+    }
+
+    public void mainMenu(){
+        //TODO
+    }
+
+    public void startGame(){
+        //TODO
+    }
+
+    public void displayHighScore(){
+        //TODO
+    }
+}
