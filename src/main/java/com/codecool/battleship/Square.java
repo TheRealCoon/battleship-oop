eeeec7d0bca9ee768bee46cda65fe3d02727ea38
T.Sanyi <s.truczkai@gmail.com>
2022-10-24 19:55:40 +0200

    added Square class

---
+++ src/main/java/com/codecool/battleship/Square.java
@@ -0,0 +1,23 @@
+package com.codecool.battleship;
+
+public class Square {
+    private int y;
+    private int x;
+
+    private SquareStatus status;
+
+    public Square(int y, int x, SquareStatus status){
+        this.y = y;
+        this.x = x;
+        this.status = status;
+    }
+
+    public SquareStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return  "y = "  + y + ", x = " + x;
+    }
+}
