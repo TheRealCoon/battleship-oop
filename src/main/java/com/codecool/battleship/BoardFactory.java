13fd32a80cd0f20392850bdaa438566370a0f825
T.Sanyi <s.truczkai@gmail.com>
2022-10-24 20:18:08 +0200

    added BoardFactory

---
+++ src/main/java/com/codecool/battleship/BoardFactory.java
@@ -0,0 +1,23 @@
+package com.codecool.battleship;
+
+public class BoardFactory {
+    private Board board;
+
+    //TODO decide which constructor to use: - Create board here,
+    // or create board somewhere else, and just pass it to the constructor
+    public BoardFactory(int size){
+        board = new Board(size);
+    }
+
+    public BoardFactory(Board board) {
+        this.board = board;
+    }
+
+    public void randomPlacement(){
+        //TODO
+    }
+
+    public void manualPlacement(){
+        //TODO probably this should receive an 'Input' instance as parameter
+    }
+}
