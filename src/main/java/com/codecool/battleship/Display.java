2753073c0f29f8c349d1648067fa429226325f68
T.Sanyi <s.truczkai@gmail.com>
2022-10-24 19:20:47 +0200

    added Display class

---
+++ src/main/java/com/codecool/battleship/Display.java
@@ -0,0 +1,15 @@
+package com.codecool.battleship;
+
+public class Display {
+    Display(){}
+
+    public void printMenu(String label, String[] menu){
+        //TODO prints any menu given
+    }
+    public void printBoard(String[][] board /* or char[][] board */){
+        //TODO receives String/char board
+    }
+    public void printGameMessage(String msg){
+        System.out.println(msg);
+    }
+}
