fd58bd411375a0cd32f80f35d1083d45258e1f36
T.Sanyi <s.truczkai@gmail.com>
2022-10-24 19:39:51 +0200

    added Game class

---
+++ src/main/java/com/codecool/battleship/Game.java
@@ -0,0 +1,22 @@
+package com.codecool.battleship;
+
+public class Game {
+    private Player player1;
+    private Player player2;
+
+    Game(){}
+
+    public String gameModeMenu(){
+        //TODO
+        return "";
+    }
+
+    public void play(){
+        //TODO
+    }
+
+    private boolean hasWon(){
+        //TODO
+        return false;
+    }
+}
