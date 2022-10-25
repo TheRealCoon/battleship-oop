4c016e9381daa332ce0311b343a735281d6105e7
Jutsz <judit.fulmer@gmail.com>
2022-10-25 13:33:09 +0200

    add createEmptyBoard method in Board class

--- src/main/java/com/codecool/battleship/board/Board.java
+++ src/main/java/com/codecool/battleship/board/Board.java
@@ -7,6 +7,14 @@ public class Board {
     public Board(int size){
         ocean = new Square[size][size];
     }
+
+    public void createEmptyBoard(){
+        for (int i = 0; i < ocean.length; i++) {
+            for (int j = 0; j < ocean.length; j++) {
+                ocean[i][j]=new Square(i, j, SquareStatus.EMPTY);
+            }
+        }
+    }
     public boolean isPlacementOk(Square square, Ship ship){
         //TODO
         return false;
