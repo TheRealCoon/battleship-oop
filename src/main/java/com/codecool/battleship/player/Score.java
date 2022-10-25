e619bd029fcd2271d3db5f79fe780616866d09e3
T.Sanyi <s.truczkai@gmail.com>
2022-10-25 18:12:34 +0200

    created Score class

---
+++ src/main/java/com/codecool/battleship/player/Score.java
@@ -0,0 +1,22 @@
+package com.codecool.battleship.player;
+
+public class Score {
+    private int value;
+    private String playerName;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+}
