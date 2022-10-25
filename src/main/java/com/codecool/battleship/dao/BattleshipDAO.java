2a84830fa408230036ed5cca1daa0f54ff4e5d3d
T.Sanyi <s.truczkai@gmail.com>
2022-10-25 21:13:47 +0200

    Highscore printing works, there will be some additions later

--- src/main/java/com/codecool/battleship/dao/BattleshipDAO.java
+++ src/main/java/com/codecool/battleship/dao/BattleshipDAO.java
@@ -7,24 +7,26 @@ import static com.codecool.battleship.utils.Constans.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public class BattleshipDAO {
 
     public static List<Score> readHighScoreFromFile() throws IOException {
-        List<Score> highScore = new ArrayList<>();
-        BufferedReader br;
         String line;
         String[] values;
-        br = new BufferedReader(new FileReader(DATA_FILE));
-        if (br.readLine() == null || br.readLine().isEmpty())
-            throw new NullPointerException("There are no entries in High Scores file!");
+        List<Score> highScore = new ArrayList<>();
+        if (isFileEmpty(DATA_FILE)) throw new NullPointerException("There are no entries in High Scores file!");
+        BufferedReader br = new BufferedReader(new FileReader(DATA_FILE));
         while ((line = br.readLine()) != null) {
             values = line.split(COLUMN_SEPARATOR);
             Score record = new Score(values[NAME_TABLE_INDEX], Integer.parseInt(values[SCORE_TABLE_INDEX]));
             highScore.add(record);
         }
         return highScore;
+    }
+
+    private static boolean isFileEmpty(String dataFile) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(dataFile));
+        return (br.readLine()==null || br.readLine().isEmpty());
     }
 
     public static void writeHighScoreToFile(List<Score> highScore) throws IOException {
