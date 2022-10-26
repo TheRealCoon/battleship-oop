6c3c49c27e9634642ed0a5899e29ce561c4a6b10
Jutsz <judit.fulmer@gmail.com>
2022-10-26 15:59:58 +0200

    implement isPlacementOk method

--- src/main/java/com/codecool/battleship/board/Board.java
+++ src/main/java/com/codecool/battleship/board/Board.java
@@ -11,16 +11,17 @@ public class Board {
     private Square[][] ocean;
     private int size;
     private Set<Square> shipSquares;
-    public Board(int size){
+
+    public Board(int size) {
         this.size = size;
         ocean = new Square[this.size][this.size];
         shipSquares = new HashSet<>();
     }
 
-    public Square[][] createEmptyBoard(){
+    public Square[][] createEmptyBoard() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                ocean[i][j]=new Square(i, j, SquareStatus.EMPTY);
+                ocean[i][j] = new Square(i, j, SquareStatus.EMPTY);
             }
         }
         return ocean;
@@ -28,6 +29,10 @@ public class Board {
 
     public Square[][] getOcean() {
         return ocean;
+    }
+
+    public void setOcean(Square[][] ocean) {
+        this.ocean = ocean;
     }
 
     public Set<Square> getShipSquares() {
@@ -38,8 +43,12 @@ public class Board {
         this.shipSquares = shipSquares;
     }
 
-    public boolean isPlacementOk(Square square, Ship ship){
-        //TODO
+    public boolean isPlacementOk(Ship ship) {
+        for (Square body:ship.getBody()) {
+            return ((body.getX() <= size && body.getY() <= size)
+                    && ocean[body.getX()][body.getY()].getStatus().equals(SquareStatus.EMPTY)
+                    && !ocean[body.getX()][body.getY()].getStatus().equals(SquareStatus.NEIGHBOUR));
+        }
         return false;
     }
 }
