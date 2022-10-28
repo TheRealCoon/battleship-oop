58d85d10178cf10ba56eb2d74f8b6bb39c24b386
rabbitST <biroattila.eu@gmail.com>
2022-10-28 08:41:38 +0200

    add isValidCoordinate() method to Input.java

--- src/main/java/com/codecool/battleship/Input.java
+++ src/main/java/com/codecool/battleship/Input.java
@@ -13,11 +13,20 @@ public class Input {
     public Input() {
     }
 
-    
+
 
     public String getInput() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
+
+    public boolean isValidCoordiate(String input) {
+        String X = input.substring(0, 1);
+        int XintValue = (int) X.toUpperCase().charAt(0);
+        if(XintValue<65 || XintValue>74) return false;
+        String Y = input.substring(1);
+
+        return Y.chars().allMatch(Character::isDigit) && Integer.parseInt(Y)>=0 && Integer.parseInt(Y)<=10;
     }
 
 
