ddd0ab393e6d17876c891b8acdb455db3371b2fa
rabbitST <biroattila.eu@gmail.com>
2022-10-29 16:34:59 +0200

    GUI/HighScore implemented BattleshipDao

--- src/main/java/com/codecool/battleship/HighScore.java
+++ src/main/java/com/codecool/battleship/HighScore.java
@@ -1,12 +1,20 @@
 package com.codecool.battleship;
 
+import com.codecool.battleship.dao.BattleshipDAO;
+import com.codecool.battleship.player.Score;
+import com.codecool.battleship.utils.Display;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class HighScore extends JLayeredPane {
     JLabel highScoreLabel;
+    Score[] highScore;
+    Display errorMsg=new Display();
 
     HighScore(BoardPane display) {
         setBounds(0, 0, 1600, 800);
@@ -14,23 +22,44 @@ public class HighScore extends JLayeredPane {
         setVisible(false);
         setLayout(null);
 
-        highScoreLabel= new JLabel();
-        highScoreLabel.setBounds(200,-150,1600,800);
+        highScoreLabel = new JLabel();
+        highScoreLabel.setBounds(200, 0, 1600, 800);
         highScoreLabel.setFont(new Font("Lucida", Font.BOLD, 30));
         highScoreLabel.setForeground(Color.WHITE);
-        highScoreLabel.setText("<html><table>" +
-                "<tr><td style='width:50px'>N. </td><td style='width:150px'>Name</td><td style='width:50px'>   " +
-                "Points</td></tr>" +
-                "<tr><td>1.</td><td> Juci</td><td>   56</td></tr>" +
-                "<tr><td>2.</td><td> Sanyi </td><td> 45</td></tr>" +
-                "<tr><td>3.</td><td> Dani </td><td> 33</td></tr>" +
-                "<tr><td>4.</td><td> Attila </td><td>25</td></tr> </table></html>");
+        try {
+            highScore = BattleshipDAO.readHighScoreFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        } catch (NullPointerException e) {
+            errorMsg.printErrorMessage(e.getMessage());
+            return;
+        }
+
+        int row=0;
+        StringBuilder highScoreTable=new StringBuilder();
+        highScoreTable.append("<html><table><tr><td style='width:50px'>Place</td><td " +
+                "style='width:210px'>Name</td><td " +
+                "style='width:50px'>Points</td></tr>");
+        for (Score field: highScore
+        ) {
+            row++;
+            highScoreTable.append("<tr><td>");
+            highScoreTable.append(row);
+            highScoreTable.append("</td><td>");
+            highScoreTable.append(field.getPlayerName().replaceAll("([^A-z ])",""));
+            highScoreTable.append("</td><td>");
+            highScoreTable.append(field.getValue());
+            highScoreTable.append("</td></tr>");
+        }
+        highScoreTable.append("</table></html>");
+        highScoreLabel.setText(highScoreTable.toString());
         add(highScoreLabel, Integer.valueOf(2));
         highScoreLabel.setVisible(true);
 
-        ImageIcon highScoreBackgroundIMG= new ImageIcon("src/main/java/com/codecool/battleship/iconsOfComponents/HSBackground.png");
-        JLabel backGround= new JLabel(highScoreBackgroundIMG);
-        backGround.setBounds(0,0,1600,800);
+        ImageIcon highScoreBackgroundIMG = new ImageIcon("src/main/java/com/codecool/battleship/iconsOfComponents/HSBackground.png");
+        JLabel backGround = new JLabel(highScoreBackgroundIMG);
+        backGround.setBounds(0, 0, 1600, 800);
         backGround.setLayout(null);
         backGround.setVisible(true);
         add(backGround, Integer.valueOf(1));
@@ -38,7 +67,7 @@ public class HighScore extends JLayeredPane {
         JButton buttonMainMenu = new JButton("Main menu");
         buttonMainMenu.setBounds(260, 670, 300, 42);
         buttonMainMenu.setFont(new Font("Lucida", Font.BOLD, 30));
-        buttonMainMenu.setMargin(new Insets(2,2,2,2));
+        buttonMainMenu.setMargin(new Insets(2, 2, 2, 2));
         buttonMainMenu.setVisible(true);
         this.add(buttonMainMenu, Integer.valueOf(2));
 
