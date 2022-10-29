e93dad84a88b20f3cc645840fb1468d3af62891f
rabbitST <biroattila.eu@gmail.com>
2022-10-29 14:34:00 +0200

    add turnInputIntoSquare() and guiInputToPositionInPixel() methods to utils/Input

--- src/main/java/com/codecool/battleship/utils/Input.java
+++ src/main/java/com/codecool/battleship/utils/Input.java
@@ -1,10 +1,12 @@
 package com.codecool.battleship.utils;
 
 import com.codecool.battleship.board.Square;
+import com.codecool.battleship.board.SquareStatus;
 
 import java.util.Scanner;
 
-import static com.codecool.battleship.utils.Constans.BOARD_SIZE;
+import static com.codecool.battleship.utils.Constans.*;
+import static com.codecool.battleship.utils.Constans.FIELD_SIZE_IN_PIXEL;
 
 
 public class Input {
@@ -26,6 +28,18 @@ public class Input {
         if (XintValue < 'A' || XintValue > 'A' + BOARD_SIZE - 1) return false;
         String Y = input.substring(1);
         return Y.chars().allMatch(Character::isDigit) && Integer.parseInt(Y) >= 1 && Integer.parseInt(Y) <= BOARD_SIZE;
+    }
+
+    public Square turnInputIntoSquare(String coordinate, SquareStatus status){
+        int x =(coordinate.substring(0,1).toUpperCase().charAt(0)-CHARACTER_TO_BOARD_CORRECTION)*FIELD_SIZE_IN_PIXEL;
+        int y = Integer.parseInt(coordinate.substring(1))*FIELD_SIZE_IN_PIXEL;
+        return new Square(y,x,status);
+    }
+
+    public int[] guiInputToPositionInPixel(String coordinate){
+        int x =(coordinate.substring(0,1).toUpperCase().charAt(0)-CHARACTER_TO_BOARD_CORRECTION)*FIELD_SIZE_IN_PIXEL;
+        int y = Integer.parseInt(coordinate.substring(1))*FIELD_SIZE_IN_PIXEL;
+        return new int[]{x,y};
     }
 
     public String readInput(String msg) {
