c5122375e64e501929589bca2fe7e42564dd26c3
T.Sanyi <s.truczkai@gmail.com>
2022-11-02 02:08:05 +0100

    managed to show background part2

--- src/main/java/com/codecool/battleship/GUI/GUI.java
+++ src/main/java/com/codecool/battleship/GUI/GUI.java
@@ -3,6 +3,7 @@ package com.codecool.battleship.GUI;
 import static com.codecool.battleship.utils.Constants.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class GUI extends JFrame {
@@ -10,46 +11,44 @@ public class GUI extends JFrame {
 
     public GUI() {
 
-//        layeredPane = initLayeredPane();
+        layeredPane = initLayeredPane();
 
         ImageIcon icon = new ImageIcon(ICONS_DIRECTORY + "gameIcon.png");
-        ImageIcon icon2 = new ImageIcon(ICONS_DIRECTORY + "menuBackGround.png");
-
-        JLabel label = new JLabel();
-        label.setSize(GUI_WIDTH, GUI_HEIGHT);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setIcon(icon2);
-
+//        ImageIcon icon2 = new ImageIcon(ICONS_DIRECTORY + "menuBackGround.png");
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
-//        this.setBounds(GUI_STARTING_X, GUI_STARTING_Y, GUI_WIDTH, GUI_HEIGHT);
+        this.setSize(GUI_WIDTH, GUI_HEIGHT);
 //        this.setResizable(false);
-//        this.setUndecorated(true);
-//        this.setOpacity(0.8f);
-
         this.setTitle(GAME_TITLE);
         this.setIconImage(icon.getImage());
         this.getContentPane().setBackground(new Color(0, 130, 130));
 
-        this.add(label);
-//        this.add(layeredPane);
+
+        this.add(layeredPane, BorderLayout.CENTER);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
     private JLayeredPane initLayeredPane() {
-        JLayeredPane layeredPane = new JLayeredPane();
+
 
         Menu menuPanel = new Menu();
 //        GamePanel gamePanel = new GamePanel();
 //        HighScorePanel highScorePanel = new HighScorePanel();
 
-        layeredPane.setBounds(0, 0, GUI_WIDTH, GUI_HEIGHT);
-        layeredPane.add(menuPanel, Integer.valueOf(2));
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setLayout(new BorderLayout());
+        layeredPane.setPreferredSize(this.getSize());
+//        layeredPane.setBounds(0, 0, GUI_WIDTH, GUI_HEIGHT);
+        layeredPane.add(menuPanel, BorderLayout.CENTER);
+        layeredPane.setLayer(menuPanel, JLayeredPane.DRAG_LAYER);
+        Border border = BorderFactory.createLineBorder(Color.red, 3);
+        layeredPane.setBorder(border);
+
 //        layeredPane.add(gamePanel, Integer.valueOf(1));
 //        layeredPane.add(highScorePanel, Integer.valueOf(0));;
-        layeredPane.setVisible(true);
+//        layeredPane.setVisible(true);
         return layeredPane;
     }
 
--- src/main/java/com/codecool/battleship/GUI/Menu.java
+++ src/main/java/com/codecool/battleship/GUI/Menu.java
@@ -1,6 +1,9 @@
 package com.codecool.battleship.GUI;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
+import java.awt.*;
 
 import static com.codecool.battleship.utils.Constants.*;
 
@@ -8,15 +11,17 @@ public class Menu extends JLayeredPane {
     JPanel mainMenu;
     JPanel gameModeMenu;
     JPanel shipPlacementMenu;
-    JLabel lblbackground;
+    JLabel lblBackground;
 
     public Menu() {
-//        mainMenu = new JPanel();
-//        //TODO find out best layout
-//        mainMenu.setLayout(null);
-////        mainMenu.setBounds(0,0,1600,800);
-////        mainMenu.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 50));
-////        mainMenu.setVisible(true);
+        mainMenu = new JPanel();
+        //TODO find out best layout
+        mainMenu.setLayout(null);
+        mainMenu.setBounds(500,0,600,800);
+//       mainMenu.setSize(600,800);
+//        mainMenu.setOpaque(false);
+//        mainMenu.setVisible(true);
+
 //        gameModeMenu = new JPanel();
 //        //TODO find out best layout
 //        gameModeMenu.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 50));
@@ -25,16 +30,23 @@ public class Menu extends JLayeredPane {
 //        shipPlacementMenu.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 50));
 
         ImageIcon backgroundImage = new ImageIcon(ICONS_DIRECTORY + "menuBackground.png");
-        lblbackground = new JLabel(backgroundImage);
-        lblbackground.setBounds(0,0, GUI_WIDTH, GUI_HEIGHT);
+        lblBackground = new JLabel(backgroundImage);
+        lblBackground.setLayout(new BorderLayout());
+//        lblBackground.setSize(GUI_WIDTH, GUI_HEIGHT);
+        lblBackground.setVerticalAlignment(JLabel.CENTER);
+        lblBackground.setHorizontalAlignment(JLabel.CENTER);
 
 
-        this.add(lblbackground, Integer.valueOf(0));
+
+this.setLayout(new BorderLayout());
+//        this.setPreferredSize(new Dimension(GUI_WIDTH, GUI_HEIGHT));
+        this.add(lblBackground, BorderLayout.CENTER);
+        this.setLayer(lblBackground, JLayeredPane.DEFAULT_LAYER);
 //        this.add(shipPlacementMenu, Integer.valueOf(1));
 //        this.add(gameModeMenu, Integer.valueOf(2));
-//        this.add(mainMenu, Integer.valueOf(3));
-        this.setOpaque(true);
-        this.setVisible(true);
+//        this.add(mainMenu);
+        this.setLayer(mainMenu, JLayeredPane.DRAG_LAYER);
+
 
     }
 }
